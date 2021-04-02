b40f707b59f4f284981b3f152b4434d49786128a
tonyvives <vivestony1@gmail.com>
2021-04-01 23:38:20 -0600

    finishing views

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,4 +1,5 @@
 import { videos } from "../db"
+import routes from "../routes"
 
 export const home = (req, res) => {
     res.render("home", { pageTitle: "Home", videos });
@@ -8,11 +9,26 @@ export const search = (req, res) => {
     const {
         query: { term: searchingBy }
     } = req;
-    res.render("search", { pageTitle: "search", searchingBy });
+    res.render("search", { pageTitle: "search", searchingBy, videos });
 };
 
 
-export const upload = (req, res) => res.render("upload");
-export const videoDetail = (req, res) => res.render("viddetail");
+export const getUpload = (req, res) => {
+    res.render("upload", { pageTitle: "Upload" });
+}
+
+export const postUpload = (req, res) => {
+    const {
+        body: {
+            file,
+            title,
+            description
+        }
+    } = req;
+    // To do: Upload and save video
+    res.redirect(routes.videoDetail(324394));
+};
+
+export const videoDetail = (req, res) => res.render("videoDetail");
 export const editVideo = (req, res) => res.render("editVideo");
 export const deleteVideo = (req, res) => res.render("deleteVideo");
--- db.js
+++ db.js
@@ -3,8 +3,8 @@ export const videos = [
         id: 324394,
         title: "Video awesome",
         description: "This is something i love",
-        vides: 23,
-        videoFile: "https://achive.org/details/bigbuckbunny_124",
+        views: 23,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
         creator: {
             id: 23432423,
             name: "Juan",
@@ -15,8 +15,8 @@ export const videos = [
         id: 234234,
         title: "Video 2",
         description: "This is something i love",
-        vides: 23,
-        videoFile: "https://achive.org/details/bigbuckbunny_124",
+        views: 23,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
         creator: {
             id: 23432423,
             name: "Juan",
@@ -27,8 +27,8 @@ export const videos = [
         id: 23442423,
         title: "Video 3",
         description: "This is something i love",
-        vides: 23,
-        videoFile: "https://achive.org/details/bigbuckbunny_124",
+        views: 23,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
         creator: {
             id: 2323432423,
             name: "Juan",
@@ -39,8 +39,8 @@ export const videos = [
         id: 23424,
         title: "Video 4",
         description: "This is something i love",
-        vides: 23,
-        videoFile: "https://achive.org/details/bigbuckbunny_124",
+        views: 23,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
         creator: {
             id: 232344,
             name: "Juan",
